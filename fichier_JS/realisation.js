e1133976759473571854b469017220e53099d410
M-Kossy <doitupe@gmail.com>
2021-04-16 18:55:42 +0200

    amélioration du site

--- fichier_JS/realisation.js
+++ fichier_JS/realisation.js
@@ -3,24 +3,14 @@ Go_Slide(position);
 
 function Go_Slide(position) {
   var i;
-  var slide = document.getElementsByClassName("Contenu_Slide");
-  var point = document.getElementsByClassName("point");
+  var slide = document.getElementsByClassName("description_project");
+  var point = document.getElementsByClassName("title_project");
   for (i = 0; i < slide.length; i++) {
       slide[i].style.display = "none";
   }
   for (i = 0; i < point.length; i++) {
-      point[i].className = point[i].className.replace(" active", "");
+      point[i].className = point[i].className.replace(" visible", "");
   }
   slide[position-1].style.display = "block";
-  point[position-1].className += " active";
-}
-
-function Go_Web() {
-  document.getElementById("Site_TPE").style.display = "block";
-  document.getElementById("img_TPE").style.opacity = "0.1";
-}
-
-function Not_Go_Web() {
-  document.getElementById("Site_TPE").style.display = "none";
-  document.getElementById("img_TPE").style.opacity = "1";
+  point[position-1].className += " visible";
 }
